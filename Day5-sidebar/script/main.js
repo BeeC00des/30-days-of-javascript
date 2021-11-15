d4d66371e4ca603b755bea68d0c2fb1d7d7dfe33
Ajayi Bolarinwa.C <Ajayibcd@gmail.com>
2021-11-15 18:44:44 +0100

    -sidebar logic

---
+++ Day5-sidebar/script/main.js
@@ -0,0 +1,12 @@
+// open btn click action
+
+function openSideBar() {
+  document.getElementById("sideBar").style.width ="250px";
+  document.getElementById("main").style.marginLeft  ="250px";
+}
+
+//close  btn click action
+function closeSideBar() {
+  document.getElementById("sideBar").style.width ="0";
+  document.getElementById("main").style.marginLeft  ="0";
+}
